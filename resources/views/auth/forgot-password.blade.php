47d2993493555781b64cbeb2b2deacd3b4bae476
Diogo <DiogoRodriguesTwo@hotmail.com>
2025-10-27 11:13:19 -0300

    Update forgot-password.blade.php

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,85 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>Forgot Password &mdash; Stisla</title>
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+  <!-- General CSS Files -->
+  <link rel="stylesheet" href="{{ asset('backend/assets/modules/bootstrap/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('backend/assets/modules/fontawesome/css/all.min.css') }}">
 
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
+  <!-- CSS Libraries -->
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="{{ asset('backend/assets/css/style.css') }}">
+  <link rel="stylesheet" href="{{ asset('backend/assets/css/components.css') }}">
+<!-- Start GA -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-94034622-3');
+</script>
+<!-- /END GA --></head>
+
+<body>
+  <div id="app">
+    <section class="section">
+      <div class="container mt-5">
+        <div class="row">
+          <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+            <div class="login-brand">
+              <img src="{{ asset('backend/assets/img/LOGO.png ') }}" alt="logo" width="200" class="shadow-light">
+            </div>
+
+            <div class="card card-primary">
+              <div class="card-header"><h4>Forgot Password</h4></div>
+
+              <div class="card-body">
+                <p class="text-muted">We will send a link to reset your password</p>
+               <form method="POST" action="{{ route('password.email') }}">
+                @csrf
+
+                  <div class="form-group">
+                    <label for="email">Email</label>
+                    <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                  </div>
+
+                  <div class="form-group">
+                     <x-primary-button style="background-color: green;">
+                         {{ __('Email Password Reset Link') }}
+                     </x-primary-button>
+                  </div>
+                </form>
+              </div>
+            </div>
+              Criado por &copy; Eletro Descartes 2025
+            </div>
+          </div>
         </div>
+      </div>
+    </section>
+  </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+  <!-- General JS Scripts -->
+  <script src="{{ asset('backend/assets/modules/jquery.min.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/popper.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/tooltip.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/bootstrap/js/bootstrap.min.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/moment.min.js') }}"></script>
+  <script src="{{ asset('backend/assets/js/stisla.js') }}"></script>
+  
+  <!-- JS Libraies -->
+
+  <!-- Page Specific JS File -->
+  
+  <!-- Template JS File -->
+  <script src="{{ asset('backend/assets/js/scripts.js') }}"></script>
+  <script src="{{ asset('backend/assets/js/custom.js') }}"></script>
+</body>
+</html>
